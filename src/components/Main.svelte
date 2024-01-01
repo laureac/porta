6e5b9531ef80b3f8f993629dd7617f3ef3084a23
laureac <laure.a.claret@gmail.com>
2024-01-01 23:34:01 +0100

    refatcor, add style

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,27 +1,49 @@
 <script>
-    import { onMount } from 'svelte';
-    import { supabase } from '../lib/initSupabase.js';
+  import { onMount } from "svelte";
+  import { fetchProjects } from "$lib/index";
 
-    let allProjects = []; 
+  let projects = [];
 
-    onMount(async () => {
-        let { data: projects, error } = await supabase
-        .from('projects')
-        .select('*')
-
-        if (error) {
-            console.error('Error:', error);
-        } else {
-            allProjects = projects
-            console.log(projects);
-        }
-    });
+  onMount(async () => {
+    try {
+      projects = await fetchProjects();
+    } catch (error) {
+      console.error("Error fetching projects:", error);
+    }
+  });
 </script>
 
-<div>
-  {#each allProjects as project}
-    <div>
-      <h2>{project.name}</h2>
-    </div>
-  {/each}
+<div class="container mx-auto p-4">
+  <div class="flex gap-4 justify-end pb-8">
+    <i class="fa-brands fa-github"></i>
+    <i class="fa-brands fa-linkedin"></i>
+    <i class="fa-solid fa-link"></i>
+  </div>
+  <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+    {#each projects as project}
+      <div
+        class="border border-gray-200 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300"
+      >
+        {#if project.link}
+          <a href={project.link} target="_blank" class="block">
+            <img
+              src={project.image_url}
+              alt={project.name}
+              class="w-full h-48 object-cover"
+            />
+            <!-- <div class="p-4 flex justify-between items-center">
+            <h2 class="font-semibold text-lg mb-2">{project.name}</h2>
+            <i class="fa-solid fa-arrow-right"></i>
+          </div> -->
+          </a>
+        {:else}
+          <img
+            src={project.image_url}
+            alt={project.name}
+            class="w-full h-48 object-cover"
+          />
+        {/if}
+      </div>
+    {/each}
+  </div>
 </div>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,7 @@
-// place files you want to import through the `$lib` alias in this folder.
+import { supabase } from "./initSupabase";
+
+export async function fetchProjects() {
+  let { data: projects, error } = await supabase.from("projects").select("*");
+  if (error) throw error;
+  return projects;
+}
