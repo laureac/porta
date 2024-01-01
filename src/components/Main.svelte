bdc7ded5723924ac1d636654834ef24e53f401d3
laureac <laure.a.claret@gmail.com>
2024-01-01 22:43:00 +0100

    setup DB

---
+++ src/components/Main.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { onMount } from 'svelte';
+    import { supabase } from '../lib/initSupabase.js';
+
+    let allProjects = []; 
+
+    onMount(async () => {
+        let { data: projects, error } = await supabase
+        .from('projects')
+        .select('*')
+
+        if (error) {
+            console.error('Error:', error);
+        } else {
+            allProjects = projects
+            console.log(projects);
+        }
+    });
+</script>
+
+<div>
+  {#each allProjects as project}
+    <div>
+      <h2>{project.name}</h2>
+    </div>
+  {/each}
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
+    <slot />
+</div>
