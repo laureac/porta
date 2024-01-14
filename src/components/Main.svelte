c7bc381bc8ddb7c988972321301332c579570a2f
laureac <laure.a.claret@gmail.com>
2024-01-14 19:25:12 +0100

    add style

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { fetchProjects } from "$lib/index";
+  import Menu from './Menu.svelte';
 
   let projects = [];
 
@@ -14,71 +15,31 @@
   });
 </script>
 
-<div class="container mx-auto p-4">
-  <div class="grid grid-cols-3 gap-4">
-    <div class="col-span-1">
-      <div>
-        <img
-          src="https://res.cloudinary.com/dv43fkmcn/image/upload/v1705174145/PXL_20231230_143445453_itawnh.jpg"
-          alt="Laure Claret"
-          class="h-48 w-48 object-cover"
-        />
-        <h2>Laure Claret</h2>
-        <p>@laureac</p>
-        <p>+33 7 44 94 74 07</p>
-        <div class="flex gap-4 pb-8">
-          <i class="fa-brands fa-github"></i>
-          <i class="fa-brands fa-linkedin"></i>
-          <i class="fa-solid fa-link"></i>
-        </div>
-      </div>
-    </div>
-    <div class="col-span-2">
-      <div class="masonry">
+<div class="container mx-auto px-8 py-10">
+  <div class="grid sm:grid-cols-4 gap-4">
+    <Menu/>
+    <div class="col-span-4 sm:col-span-3">
+      <div class="grid sm:grid-cols-3 gap-4">
         {#each projects as project}
           <div
             class="border border-gray-200 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition duration-300"
           >
-            {#if project.link}
-              <a href={project.link} target="_blank" class="block">
-                <img
-                  src={project.image_url}
-                  alt={project.name}
-                  class="w-full h-80 object-cover"
-                />
-                <div class="p-4 flex justify-between items-center">
-                  <h2 class="text-md mb-2">{project.name}</h2>
-                  <p>{project.description ?? ""}</p>
-                </div>
-              </a>
-            {:else}
+            <a href={project.link ?? "#"} target="_blank" class="block">
               <img
                 src={project.image_url}
                 alt={project.name}
                 class="w-full h-80 object-cover"
               />
-              <div class="p-4 flex justify-between items-center">
+              <div class="p-4 flex justify-between flex-col">
                 <h2 class="text-md mb-2">{project.name}</h2>
-                <p>{project.description ?? ""}</p>
+                <p class="text-slate-500 text-sm">
+                  {project.description ?? ""}
+                </p>
               </div>
-            {/if}
+            </a>
           </div>
         {/each}
       </div>
     </div>
   </div>
 </div>
-
-<style>
-  .masonry {
-    display: grid;
-    gap: 10px;
-    grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
-    grid-template-rows: masonry;
-  }
-
-  .masonry > div {
-    grid-column: span 2;
-    grid-row: span 2;
-  }
-</style>
---
+++ src/components/Menu.svelte
@@ -0,0 +1,69 @@
+<script>
+  let socialLinks = [
+            { url: 'https://devpost.com/laureac/', iconClass: 'fa-solid fa-link' },
+            { url: 'https://github.com/laureac', iconClass: 'fab fa-github' },
+            { url: 'https://www.linkedin.com/in/laure-claret/', iconClass: 'fab fa-linkedin' },
+            { url: 'mailto:laure.a.claret@gmail.com', iconClass: 'fa-regular fa-envelope' },
+          ];
+
+  let isMenuOpen = false;
+  
+</script>
+
+<div class="menu">
+  <div class="block sm:hidden">
+    <button class="hamburger my-1" on:click={() => isMenuOpen = !isMenuOpen}>
+      {#if isMenuOpen}
+        <i class="fa fa-times text-xl"></i>
+      {:else}
+        <i class="fa fa-bars text-xl"></i>
+      {/if}
+    </button>
+    {#if isMenuOpen}
+        <div class="flex flex-col gap-5">
+        <img
+          src="https://res.cloudinary.com/dv43fkmcn/image/upload/v1705174145/PXL_20231230_143445453_itawnh.jpg"
+          alt="Laure Claret"
+          class="h-48 w-3/4 object-cover"
+        />
+        <h2 class="text-lg">Laure Claret</h2>
+        <div class="text-sm flex flex-col gap-2 text-slate-500"> 
+          <p>Hey! I'm Laure, and I develop lots of random things. I do a lot of other activities too!</p>
+          <p>@laureac</p>
+          <p>+33 7 44 94 74 07</p>
+          <p>laure.a.claret@gmail.com</p>
+        </div>
+        <div class="flex gap-4">
+          {#each socialLinks as link}
+            <a href={link.url} target="_blank" class="icon">
+              <i class={link.iconClass}></i>
+            </a>
+          {/each}
+        </div>
+      </div>
+    {/if}
+    </div>
+  <div class="hidden sm:block col-span-1">
+    <div class="flex flex-col gap-5">
+      <img
+        src="https://res.cloudinary.com/dv43fkmcn/image/upload/v1705174145/PXL_20231230_143445453_itawnh.jpg"
+        alt="Laure Claret"
+        class="h-48 w-3/4 object-cover"
+      />
+      <h2 class="text-lg">Laure Claret</h2>
+      <div class="text-sm flex flex-col gap-2 text-slate-500"> 
+        <p>Hey! I'm Laure, and I develop lots of random things. I do a lot of other activities too!</p>
+        <p>@laureac</p>
+        <p>+33 7 44 94 74 07</p>
+        <p>laure.a.claret@gmail.com</p>
+      </div>
+      <div class="flex gap-4">
+        {#each socialLinks as link}
+          <a href={link.url} target="_blank" class="icon">
+            <i class={link.iconClass}></i>
+          </a>
+        {/each}
+      </div>
+    </div>
+  </div>
+</div>
